4eb65954d08eb21036dc1a280877f1a70af2a41a
Danny Rottstegge <danny@rottstegge.net>
2021-05-04 10:13:41 +0200

    Add tailwindcss

---
+++ packages/web/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    purge: {
+        enabled: process.env.WEBPACK_DEV_SERVER === 'true' && process.argv.join(' ').indexOf('build') !== -1,
+        content: ['./src/**/*.{html,ts,scss}'],
+    },
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+};
